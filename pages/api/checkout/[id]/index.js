ccf0046fbe6e11f1cef7c3cbc90707e4261abd3a
allelos <p.paliagkas@gmail.com>
2021-06-09 00:00:09 +0300

    Unwrapped object.

--- pages/api/checkout/[id]/index.js
+++ pages/api/checkout/[id]/index.js
@@ -4,7 +4,7 @@ export default async ({ query }, res) => {
   const { id } = query
   try {
     const checkout = await getCheckout(id)
-    return checkout ? res.status(200).json({ checkout }) : res.status(410).json({})
+    return checkout ? res.status(200).json(checkout) : res.status(410).json({})
   } catch (e) {
     res.status(400).json(e)
   }
