f62b19513eece2060e7c7c84f3bacd4dcee060e9
allelos <p.paliagkas@gmail.com>
2022-05-04 23:18:16 +0300

    Changed products grid.

--- organisms/products/grid/index.js
+++ organisms/products/grid/index.js
@@ -4,14 +4,15 @@ import Flex from "@components/flex"
 const Grid = styled(Flex)`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 48px;
+  gap: 48px 4px;
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (max-width: 475px) {
-    gap: 24px;
+  @media (max-width: 425px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 24px 4px;
   }
 `
 
